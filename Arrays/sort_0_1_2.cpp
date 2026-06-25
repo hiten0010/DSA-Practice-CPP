e119fcb7f5adaa158cca5ad6b0290ebc47314cc6
Hiten Punia <hitenpuniadavrtk11a57@gmail.com>
2026-06-25 21:27:48 +0530

    Day 11: Optimal C++ solution for Sort 0s, 1s, and 2s (Dutch National Flag Algorithm)

---
+++ Arrays/sort_0_1_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int start =0;
+        int mid =0;
+        int end =arr.size()-1;
+        while(mid<=end){
+            if(arr[mid]==0){
+                swap(arr[start],arr[mid]);
+                start++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr[mid],arr[end]);
+                end--;
+            }
+        }
+    }
+};
