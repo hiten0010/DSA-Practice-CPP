aa8b209e6fc84e4209678dd807aa80094c48700e
Hiten Punia <hitenpuniadavrtk11a57@gmail.com>
2026-08-09 21:31:22 +0530

    Day 56: Solved Count Primes (Sieve of Eratosthenes)

---
+++ Math/count_primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2){
+            return 0;
+        }
+        vector<bool> check(n,true);
+        check[0]=check[1]=false;
+        for(int i =2;i*i<n;i++){
+            if(check[i]){
+                for(int j =(long long)i *i ;j<n;j += i){
+                    check[j]=false;
+                }
+            }
+        }
+        int count =0;
+        for(int i =2;i<n;i++){
+            if(check[i])count++;
+        }
+        return count;
+    }
+};
