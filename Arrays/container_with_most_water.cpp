b30aeed01c275d26ebc093ac1b2d1a6677c20392
Hiten Punia <hitenpuniadavrtk11a57@gmail.com>
2026-08-18 21:40:02 +0530

    Day 65: Solved Container With Most Water (Optimal Two-Pointer)

---
+++ Arrays/container_with_most_water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int max_water = 0;
+
+        while (left < right) {
+            int current_height = min(height[left], height[right]);
+            int current_width = right - left;
+            int current_water = current_height * current_width;
+
+            max_water = max(max_water, current_water);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max_water;
+    }
+};
