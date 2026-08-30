c1a960e99c9d7261a20c598872df45dbdecd1fde
Hiten Punia <hitenpuniadavrtk11a57@gmail.com>
2026-08-30 22:30:38 +0530

    Day 77: Solved Contiguous Array (Prefix Sum + Hash Map)

---
+++ PrefixSum/contiguous_array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+
+        int sum = 0;
+        int max_len = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += (nums[i] == 1) ? 1 : -1;
+
+            if (mp.find(sum) != mp.end()) {
+                max_len = max(max_len, i - mp[sum]);
+            } else {
+                mp[sum] = i;
+            }
+        }
+        return max_len;
+    }
+};
